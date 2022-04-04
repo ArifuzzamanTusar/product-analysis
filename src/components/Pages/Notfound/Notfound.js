73b1fc66477c3c27e239eefce56c1ddba8f2c5bf
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-04-04 22:59:17 +0600

    not found bug fix

--- src/components/Pages/Notfound/Notfound.js
+++ src/components/Pages/Notfound/Notfound.js
@@ -6,7 +6,7 @@ const Notfound = () => {
         <div style={{ minHeight: '90vh' }} >
             <Container className='text-center'>
 
-                <Image className='img-fluid ' src='assets/not-found.gif'></Image>
+                <Image className='img-fluid ' src='https://res.cloudinary.com/tusar/image/upload/v1649090972/random-projects/not-found_d3cfes.gif'></Image>
             </Container>
         </div>
     );
